0b0afab176b780374f2844cd0f555d96105cb637
nmschorr <12592910+nmschorr@users.noreply.github.com>
2020-04-12 04:51:21 -0700

    updated files

--- src/user_inputs/sender_etc.py
+++ src/user_inputs/sender_etc.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3.7
 # a place to put your lists of accounts, etc.
-
 # usage:  from src.user_inputs.input_singles import AddressSingles;  dict = AddressSingles.get_addresses()
 
 
@@ -8,37 +7,19 @@ def get_sender_etc_dict(machine):
     sender_etc_d = None
     if machine == 1:
         sender_etc_d = {
-            'sender': 'TTbKRT4vrHMQdyQCATrdu6godeo1FJWSFVVk',
-
-            # 'pw2': 'password123',
-
+            'sender': 'TTbKRT4vrHMQdyQCATrdu6godeo1FJWSFVpk',
             'pw': 'nuls123456',
-
             'buyer': 'TTSETeCA3FWQ3Y32TCFEwJvzqGbxiXNxtkzPb3z',
-
             'contract': 'TTSETeCA3FWQ3Y32TCFEwJvzqGbxiXNxtkzPb3z'
             }
-#password123
 
     elif machine == 0:
         sender_etc_d = {
-            'sender': 'TTSETeCA3FWPueNuqEcKUNoxsuKZArhTA7Q2ecZ',
-
-            'pw': 'password123',
-            # 'pw2': 'password123',
-            #
-            # 'pw': 'nuls123456',
+            'sender': 'TTSETeCA3FWPueNuqEcKUNoxsuKZArhTA7Q2ccZ',
+            'pw': 'xyzxyz123',
             'buyer': 'TTSETeCA3FeQmoc3i8393zZV9WWPU5FmMLQs317',
-
             'contract': 'TTSETeCA3FueL9cKCiDR8vAiRiGVtVCJksEsstM' #?
             }
 
     return sender_etc_d
 
-# TTSETeCA3FWPueNuqEcKUNoxsuKZArhTA7Q2ecZ  kathys
-#TTSETeCA3FeQmoc3i8393zZV9WWPU5FmMLQs317
-
-        # TTbKRT4qEYosbviWgnWLqnMghDWh1CJUgqLW  west?
-#TTSETeCA3Fdhsu91EFmTuwHpXaNfWgUDL35sZS7
-            #'sender_pw': 'password123',
-#"TTSETeCA3FWPueNuqEcKUNoxsuKZArhTA7Q2ecZ" kathy's
--- src/user_inputs/settings_main.py
+++ src/user_inputs/settings_main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3.7
 
- # 0=KathyUbuntu, 1=westteam
-
-
 def get_settings(machine):
     if machine == 0:
         chain0 = 24442
@@ -13,8 +10,8 @@ def get_settings(machine):
 
     elif machine == 1:
         chain1 = 4810
-        url31 = "http://westteam.nulstar.com:18003"
-        url41 = "http://westteam.nulstar.com:18004/jsonrpc"
+        url31 = "http://westteam.nulstar.co:18003"
+        url41 = "http://westteam.nulstar.co:18004/jsonrpc"
         settings_d = {"chain": chain1, "url3": url31, "url4": url41}
         return settings_d
 
@@ -32,6 +29,3 @@ def get_settings(machine):
         settings_d = {"chain": chain2, "url3": url32, "url4": url42}
         return settings_d
 
-        # beta.wallet.nuls.io: 47.244.186.65
-    # http://apitn2.nulscan.io
-    # https: // beta.wallet.nuls.io / api
